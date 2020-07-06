11e5b4f5e70e40ac0f88c70625b1f3b4d9c9c7bd
imolinav <imolinav@ieslavereda.es>
2020-07-06 14:45:23 +0200

    importing data.json into main component

---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import data from '../../../../data-import/data.json';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(data);
+    
+  }
+
+}
